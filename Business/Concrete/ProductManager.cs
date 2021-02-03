ec274d4730d58ad4b84006e36969e74faad9e858
Merve Aydin <08aydinmeve@gmail.com>
2021-02-03 17:42:10 +0300

    Proje dosyası ekle.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal; // DataAccess' e erişim
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public List<Product> GetAll()
+        {
+            //iş kodları
+            return _productDal.GetAll();
+        }
+    }
+}
